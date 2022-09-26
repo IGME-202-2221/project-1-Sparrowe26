cc159a97b3b93264e7bb64c4d3477cb88690c870
Amanda <89926961+Sparrowe26@users.noreply.github.com>
2022-09-25 21:30:01 -0400

    Checkpoint 2 complete

---
+++ Project_1_Shmup/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 1f;
+
+    Vector3 vehiclePosition = Vector3.zero;
+    Vector3 direction = Vector3.right;
+    Vector3 velocity = Vector3.zero;
+
+    Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vehiclePosition = transform.position;
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Velocity = direction * speed * deltaTime
+        velocity = direction * speed * Time.deltaTime;
+
+        // add velocity to position
+        vehiclePosition += velocity;
+
+        // draw vehicle at that position
+        transform.position = vehiclePosition;
+
+        if(vehiclePosition.y >= mainCam.orthographicSize)
+        {
+            vehiclePosition.y = mainCam.orthographicSize;
+        }
+        else if (vehiclePosition.y <= -mainCam.orthographicSize)
+        {
+            vehiclePosition.y = -mainCam.orthographicSize;
+        }
+        if (vehiclePosition.x >= mainCam.orthographicSize * mainCam.aspect)
+        {
+            vehiclePosition.x = mainCam.orthographicSize * mainCam.aspect;
+        }
+        else if (vehiclePosition.x <= -mainCam.orthographicSize * mainCam.aspect)
+        {
+            vehiclePosition.x = -mainCam.orthographicSize * mainCam.aspect;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
+
+        if(direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
+        }
+    }
+}
